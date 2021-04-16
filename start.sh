1270541b2f966693bac7c9dc4d44a204e04cdf79
severin <snirps@live.de>
2021-04-16 18:26:26 +0200

    fix filesize to 1 1024 MB videos

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 . venv/bin/activate
 export FLASK_APP=app/__init__.py
 export FLASK_ENV=development
-export CONF_FIELD=videogruss
+export CONF_FIELD=videomessage
 flask run --host=0.0.0.0 --port=8000
